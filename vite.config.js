b03bd2713ce9c98d43345483d22a607fd795e5b7
Okan Özdemir <ookanozdemir@gmail.com>
2025-06-05 21:19:23 +0300

    Simplify Vite config and update build script

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,11 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 
 export default defineConfig({
   base: '/goit-js-hw-11/',
-  root: 'src',
   build: {
-    outDir: resolve(__dirname, 'dist'),
+    outDir: 'dist',
     sourcemap: true,
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'src/index.html')
-      }
-    }
+    emptyOutDir: true
   },
   server: {
     port: 5173,
