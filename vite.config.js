dc6d25e8cc0d6f81f4bcf6b3e1019404316bbade
Okan Özdemir <ookanozdemir@gmail.com>
2025-06-05 21:20:26 +0300

    Update Vite config to fix build issues

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   base: '/goit-js-hw-11/',
+  root: '.',
+  publicDir: 'public',
   build: {
     outDir: 'dist',
     sourcemap: true,
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html')
+      }
+    }
   },
   server: {
     port: 5173,
